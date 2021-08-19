20b38cb3dcc9c670e53689b0eb9a611a176e0286
FaithDaka <53012890+FaithDaka@users.noreply.github.com>
2021-08-19 17:41:20 +0300

    css

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../components/Layout/Header";
+
+const index = () => {
+  return (
+    <div>
+      <Header></Header>
+    </div>
+  );
+};
+
+export default index;
