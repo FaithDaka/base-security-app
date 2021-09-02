c764e3a8236e18d9a1ee1b1a8308d58f9e1d10be
FaithDaka <53012890+FaithDaka@users.noreply.github.com>
2021-09-02 15:30:42 +0300

    dashboard

---
+++ src/pages/Reports/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/Layout';
+
+const Reports = ({children}) => {
+    return (
+        <Layout>
+            {children}
+        </Layout>
+    )
+}
+
+export default Reports;
