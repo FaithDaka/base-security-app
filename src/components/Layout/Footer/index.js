a86e2ef4d50c797abc59f523ca045e0a0aad2b3a
FaithDaka <53012890+FaithDaka@users.noreply.github.com>
2021-09-21 21:37:09 +0300

    fixed footer

--- src/components/Layout/Footer/index.js
+++ src/components/Layout/Footer/index.js
@@ -4,14 +4,13 @@ const index = () => {
   return (
     <footer className="base-footer">
       <div className="container-fluid">
-        <div className="d-flex flex-row justify-content-between">
-          <div className="col-5">
-            2021 ©
-            SailSecurity.
-          </div>
-          <div className="col-7" style={{textAlign:"right"}}>
-            <div className="text-sm-end d-none d-sm-block">
-              Developed by Sail Security
+        <div className="col-sm-12 col-xl-12">
+          <div className="d-flex flex-row justify-content-between">
+            <div className="col-xl-5 col-sm-5">2021 © SailSecurity.</div>
+            <div className="col-xl-7 col-sm-7" style={{ textAlign: "right" }}>
+              <div className="text-sm-end d-none d-sm-block">
+                Developed by Sail Security
+              </div>
             </div>
           </div>
         </div>
